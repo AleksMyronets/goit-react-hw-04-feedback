8f39ec0b90e6d0023f43aec9847563d91deb1844
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-05-22 15:41:59 +0300

    Перенесення файлів з попередньої роботи

---
+++ src/components/App/App.jsx
@@ -0,0 +1,10 @@
+import { Feedback } from '../FeedbackWidget/FeedbackWidget';
+import { Container } from "./App.styled"
+
+export const App = () => {
+  return (
+    <Container>
+      <Feedback />
+    </Container>
+  );
+};
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const FeedbackBtn = styled.button`
+  margin-left: 15px;
+  border-radius: 9px;
+  padding: 15px;
+  border: 5px;
+  box-shadow: -2px 2px 4px 1px rgba(34, 60, 80, 0.37);
+  background-color: blue;
+  color: #fff;
+  :hover{
+    background-color: red;
+  }
+`;
